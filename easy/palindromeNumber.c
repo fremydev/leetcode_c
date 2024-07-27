3cd026381150396e2d0b3ded67d151897b332c79
fremydev <fremy.dev@gmail.com>
2024-07-27 14:22:59 -0300

    adds isPalindrome exercise

---
+++ easy/palindromeNumber.c
@@ -0,0 +1,12 @@
+bool isPalindrome(int x) {
+    int original=x;
+    int num;
+long long reversed=0;
+    while(x>0){
+        num=x%10;
+
+        reversed = reversed*10+num;
+        x/=10;
+    }
+    return original==reversed;
+}
