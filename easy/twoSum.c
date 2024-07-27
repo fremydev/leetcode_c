4e39cb467c8a7371da261b1ae52f5449672287f2
fremydev <fremy.dev@gmail.com>
2024-07-27 14:22:10 -0300

    improve twoSum

--- easy/twoSum.c
+++ easy/twoSum.c
@@ -1,27 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int key;
+    int value;
+    int is_set;  // Flag to indicate if the entry is set
+} HashTableEntry;
+
+int hashFunction(int key, int size) {
+    return (key % size + size) % size;  // Handle negative keys
+}
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    struct hashTable {
-        int key;
-        int value;
-        UT_hash_handle hh;
-    } *hashTable = NULL, *item;
+    int hashTableSize = numsSize * 2;  // Double the size to reduce collisions
+    HashTableEntry* hashTable = (HashTableEntry*)calloc(hashTableSize, sizeof(HashTableEntry));
+    if (hashTable == NULL) {
+        *returnSize = 0;
+        return NULL;
+    }
 
     for (int i = 0; i < numsSize; i++) {
         int complement = target - nums[i];
-        HASH_FIND_INT(hashTable, &complement, item);
-        if (item) {
-            int* result = malloc(sizeof(int) * 2);
-            result[0] = item->value;
-            result[1] = i;
-            *returnSize = 2;
-            HASH_CLEAR(hh, hashTable);  // Free the hash table
-            return result;
+        int index = hashFunction(complement, hashTableSize);
+
+        // Linear probing for collision resolution
+        while (hashTable[index].is_set) {
+            if (hashTable[index].key == complement) {
+                int* result = (int*)malloc(sizeof(int) * 2);
+                result[0] = hashTable[index].value;
+                result[1] = i;
+                *returnSize = 2;
+                free(hashTable);
+                return result;
+            }
+            index = (index + 1) % hashTableSize;
         }
-        item = malloc(sizeof(struct hashTable));
-        item->key = nums[i];
-        item->value = i;
-        HASH_ADD_INT(hashTable, key, item);
+
+        index = hashFunction(nums[i], hashTableSize);
+        while (hashTable[index].is_set) {
+            index = (index + 1) % hashTableSize;
+        }
+
+        hashTable[index].key = nums[i];
+        hashTable[index].value = i;
+        hashTable[index].is_set = 1;
     }
+
     *returnSize = 0;
-    HASH_CLEAR(hh, hashTable);  // Free the hash table
+    free(hashTable);
     return NULL;
 }
