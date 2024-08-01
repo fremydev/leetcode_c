ff3dc3a5102f8c8947c3d3f607eac87375f3c488
fremydev <fremy.dev@gmail.com>
2024-08-01 10:58:57 -0300

    adds balancedBinaryTree exercise

---
+++ easy/balancedBinaryTree.c
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+#pragma GCC optimize("O3", "unroll-loops")
+int HeightTree(struct TreeNode* root){
+    if(!root){return 0;}
+
+    int hleft = HeightTree(root->left);
+    int hrigt = HeightTree(root->right);
+    
+    return hleft > hrigt ? (hleft + 1) : (hrigt +1);
+}
+
+bool isBalanced(struct TreeNode* root) {
+    if(!root) return true;
+
+    int tleft = HeightTree(root->left);
+    int trigt = HeightTree(root->right);
+    
+    if(abs(tleft - trigt) <= 1){
+        return isBalanced(root->left) && isBalanced(root->right);
+    }else{
+        return false; 
+    }
+}
