1b55f37ad2ab5c7ad6ecc1713c633b84179e83f4
fremydev <fremy.dev@gmail.com>
2024-08-02 21:19:03 -0300

    adds intersectionTwoLinkedLists exercise

---
+++ easy/intersectionTwoLinkedLists.c
@@ -0,0 +1,16 @@
+#pragma GCC optmize("O3", "unroll-loops")
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    if (headA == NULL || headB == NULL)
+        return NULL;
+
+    struct ListNode* A = headA;
+    struct ListNode* B = headB;
+
+    while (A != B) {
+        A = A == NULL ? headB : A->next;
+        B = B == NULL ? headA : B->next;
+    }
+
+    return A;
+    
+}
